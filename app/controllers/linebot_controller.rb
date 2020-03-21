21b5086fa49bdf7abf197202ccc8f2793f404a06
nao085 <tkn.0805.a@icloud.com>
2020-03-22 00:13:25 +0900

    fix-unexpected-end

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -25,13 +25,12 @@ class LinebotController < ApplicationController
     head :ok
   end
 
-  private
+private
 
-    def client
-      @client ||= Line::Bot::Client.new { |config|
-        config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-        config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-      }
-    end
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
